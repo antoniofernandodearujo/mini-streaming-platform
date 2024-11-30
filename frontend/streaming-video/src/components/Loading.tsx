171fd7f17f142e2499def4aa3cc72090e8bebcb0
Antonio Fernando <102163015+antoniofernandodearujo@users.noreply.github.com>
2024-11-30 17:12:53 -0300

    Update Loading.tsx

--- frontend/streaming-video/src/components/Loading.tsx
+++ frontend/streaming-video/src/components/Loading.tsx
@@ -6,8 +6,9 @@ export default function(isLoading: LoadingProps) {
     if (!isLoading) return null;
 
     return (
-        <div className="flex items-center justify-center min-h-screen bg-gray-100">
-            <div className="animate-spin rounded-full h-32 w-32 border-t-4 border-b-4 border-blue-500"></div>
+        <div className="flex items-center justify-center min-h-screen bg-black">
+            <div className="animate-spin rounded-full h-32 w-32 border-t-4 border-b-4 border-gray-200"></div>
         </div>
     );
 };
+
