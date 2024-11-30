1c2d8313aedd8876f88b0db8f737e7f2bb045f6d
Antonio Fernando <102163015+antoniofernandodearujo@users.noreply.github.com>
2024-11-30 18:01:50 -0300

    Update next.config.ts

--- frontend/streaming-video/next.config.ts
+++ frontend/streaming-video/next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  reactStrictMode: true,
+  experimental: {
+    appDir: true,  // Certifique-se de que a opção appDir está ativada
+  },
 };
 
 export default nextConfig;
